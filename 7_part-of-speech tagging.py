8f3b2cb4036123983da25eca41cd2ef04c58327d
VISHAAL21-7 <192424420.simats@saveetha.com>
2026-06-19 14:01:29 +0530

    Delete 7_part-of-speech tagging.py

--- 7_part-of-speech tagging.py
+++
@@ -1,8 +0,0 @@
-import nltk
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-
-text = "NLP is very interesting"
-tokens = nltk.word_tokenize(text)
-
-print(nltk.pos_tag(tokens))
