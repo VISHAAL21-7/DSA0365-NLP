7c39eb75da8047c4a50bb9a5f3e52fbebcc4f363
VISHAAL21-7 <192424420.simats@saveetha.com>
2026-06-19 14:00:41 +0530

    Delete 3_morphological analysis.py

--- 3_morphological analysis.py
+++
@@ -1,9 +0,0 @@
-import nltk
-from nltk.stem import WordNetLemmatizer
-nltk.download('wordnet')
-
-lemmatizer = WordNetLemmatizer()
-words = ["running", "better", "cats"]
-
-for word in words:
-    print(word, "->", lemmatizer.lemmatize(word))
